5ee024b8f9d894c45b1afed51229f5eddcb41b1b
Roselynn <worklaptop@Roselynns-MacBook-Pro.local>
2022-03-17 10:56:35 -0400

    add dependencies

--- server/app.js
+++ server/app.js
@@ -1,5 +1,13 @@
 const express = require('express');
-
+/*Server should log each request using morgan's dev format
+Server should indicate when it is listening and on which port
+Server should respond to GET requests to /?i=tt3896198 with movie data
+Server should respond to GET requests to /?i=tt3896198 with movie data without fetching from the OMDb API
+Server should respond to GET requests to /?t=baby%20driver with movie data
+Server should respond to GET requests to /?t=baby%20driver with movie data, without fetching from the OMDb API
+All tests should pass*/
+//dkddkkk
+//dddddddd
 
 const app = express();
 
